5c0bec425760f8b02346637c1760c0811e045ef8
Francis dela Cruz <franciszac.dlc@gmail.com>
2018-04-09 14:45:24 +0800

    HTML stripper; fails at nested message/rfc822

--- parser.py
+++ parser.py
@@ -1,27 +1,73 @@
 import os
 import sys
 import email
+import re
 
 #msg = email.message_from_string(open("sample/inmail.65062").read())
 #payload = msg.get_payload()
+n = 1000
 
-sample_dir = os.path.join(os.getcwd(), "sample")
+# Precompilation of regexes
+pp_html = re.compile('<[^>]+?>')
+pp_spaces = re.compile('(\s)\s+')
+
+def payload_preprocess(part):
+    raw_payload = part.get_payload()
+
+    # Preprocess if HTML
+    if part.get_content_subtype() == "html":
+        raw_payload = re.sub(pp_html, '', raw_payload)
+
+    # Special rules that I'm too lazy to properly implement
+    # Replace '=' with ' '
+    raw_payload = re.sub('=', ' ', raw_payload)
+
+    # Remove extra spaces
+    raw_payload = re.sub(pp_spaces, r'\1', raw_payload)
+
+    return raw_payload
+
+# Define directories
+sample_dir = os.path.join(os.getcwd(), "isolate")
 pp1_dir = os.path.join(os.getcwd(), "pp1")
-for f in os.listdir(sample_dir):
-  msg = email.message_from_string(open(os.path.join(sample_dir, f), encoding='utf-8', errors='replace').read())
 
-  # Open file for writing
-  fo = open(os.path.join(pp1_dir, f), "w")
+# For each of the files:
+ctr = 0
+filename = ""
+try:
+    for f in os.listdir(sample_dir):
+        filename = f # For debugging
+        msg = email.message_from_string(open(os.path.join(sample_dir, f), encoding='utf-8', errors='replace').read())
 
-  # Get the payload
-  payload = ""
-  if msg.is_multipart():
-    for part in msg.walk():
-      # Filter out certain types of content (images, etc)
-      if part.get_content_maintype() in ["text", "message"]:
-        payload += part.get_payload()
-  else:
-    payload = (msg.get_payload())
+        # Open file for writing
+        fo = open(os.path.join(pp1_dir, f), "w")
 
-  fo.write(str(payload))
-  fo.close()
+        # Get the email header
+        payload = "Subject: {}".format(msg['Subject'])
+
+        # MIME Parsing
+        if msg.is_multipart():
+            for part in msg.walk():
+                # Filter out certain types of content (images, etc)
+                # Update the whitelist sometimes
+                print(part)
+                print("===")
+                if part.get_content_maintype() in ["text", "message"]:
+                    payload += payload_preprocess(part)
+        else:
+            payload = payload_preprocess(msg)
+
+        # Write payload and close file
+        fo.write(str(payload))
+        fo.write('\n')
+        fo.close()
+
+        # Print how many have been preprocessed so far
+        ctr += 1
+        sys.stdout.write("\r{}/{}".format(ctr, n))
+        sys.stdout.flush()
+except:
+    e = sys.exc_info()[0]
+    print("\nError at {}: {}".format(filename, str(e)))
+
+print('')
