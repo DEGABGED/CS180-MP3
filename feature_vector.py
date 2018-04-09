c7d211ab0b6d768fdf287d268834f1a60ef1352a
Francis dela Cruz <franciszac.dlc@gmail.com>
2018-04-10 00:49:08 +0800

    adsf

--- feature_vector.py
+++ feature_vector.py
@@ -4,10 +4,15 @@ import re
 
 # Define directories and dictionary
 data_dir = os.path.join(os.getcwd(), "mp3data")
+training_dir = os.path.join(os.getcwd(), "sample")
 mp_dict = {}
+fv_dict = {}
 
 # Read the dictionary.txt for easy hashing
 with open(os.path.join(data_dir, "dictionary.txt")) as f:
     for line in f:
-        mp_dict[line.strip()] = 1
+        line = line.strip()
+        mp_dict[line] = 0
+        fv_dict[line] = 0
 
+# For all files, create a csv
