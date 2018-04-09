ca2e0a72a605c7093a9ca3ff6e725517c9f4e4f6
Francis dela Cruz <franciszac.dlc@gmail.com>
2018-04-09 22:49:50 +0800

    Preprocessing

--- parser.py
+++ parser.py
@@ -5,11 +5,12 @@ import re
 
 #msg = email.message_from_string(open("sample/inmail.65062").read())
 #payload = msg.get_payload()
-n = 1000
+n = 75419
 
 # Precompilation of regexes
 pp_html = re.compile('<[^>]+?>')
 pp_spaces = re.compile('(\s)\s+')
+pp_nonword = re.compile('[^a-zA-Z0-9-]+')
 
 def payload_preprocess(part):
     raw_payload = part.get_payload()
@@ -18,9 +19,8 @@ def payload_preprocess(part):
     if part.get_content_subtype() == "html":
         raw_payload = re.sub(pp_html, '', raw_payload)
 
-    # Special rules that I'm too lazy to properly implement
-    # Replace '=' with ' '
-    raw_payload = re.sub('=', ' ', raw_payload)
+    # Remove nonwords
+    raw_payload = re.sub(pp_nonword, ' ', raw_payload)
 
     # Remove extra spaces
     raw_payload = re.sub(pp_spaces, r'\1', raw_payload)
@@ -29,17 +29,12 @@ def payload_preprocess(part):
 
 # Takes in an email object, returns a string containing the parsed email
 def parse_email(msg):
-    '''
-    payload = msg.get_payload()
-
-    # If the MIME type is message/rfc822, it is read as an email within an email
-    while msg.get_content_type() == "message/rfc822":
-        msg = payload[0]
-        payload = msg.get_payload()
-        '''
+    # Get the email header
+    payload = ""
+    if msg['Subject']:
+        payload = msg['Subject']
 
     # MIME parsing
-    payload = ''
     if msg.is_multipart():
         for part in msg.walk():
             if part.get_content_maintype() in ["text", "message"] and not part.is_multipart():
@@ -50,8 +45,8 @@ def parse_email(msg):
     return payload
 
 # Define directories
-sample_dir = os.path.join(os.getcwd(), "sample")
-pp1_dir = os.path.join(os.getcwd(), "pp1")
+sample_dir = os.path.join(os.getcwd(), "preprocessed")
+pp1_dir = os.path.join(os.getcwd(), "trec07p", "data")
 
 # For each of the files:
 ctr = 0
@@ -64,23 +59,8 @@ try:
         # Open file for writing
         fo = open(os.path.join(pp1_dir, f), "w")
 
-        # Get the email header
-        payload = "Subject: {}".format(msg['Subject'])
-
-        # MIME Parsing
-        '''
-        if msg.is_multipart():
-            for part in msg.walk():
-                # Filter out certain types of content (images, etc)
-                # Update the whitelist sometimes
-                print(part)
-                print("===")
-                if part.get_content_maintype() in ["text", "message"]:
-                    payload += payload_preprocess(part)
-        else:
-            payload = payload_preprocess(msg)
-            '''
-        payload += parse_email(msg)
+        # Get the payload
+        payload = parse_email(msg)
 
         # Write payload and close file
         fo.write(str(payload))
