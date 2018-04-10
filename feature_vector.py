2225f7b7351ec3182befda667c6416f4b91a24e3
Francis dela Cruz <franciszac.dlc@gmail.com>
2018-04-11 01:34:11 +0800

    Feature vector generation

--- feature_vector.py
+++ feature_vector.py
@@ -1,18 +1,59 @@
 import os
 import sys
 import re
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Function to convert list to string
+fv_noncsv = re.compile('[\[|\]|\s]')
+
+def list2str(l):
+    return re.sub(fv_noncsv, '', str(l))
 
 # Define directories and dictionary
 data_dir = os.path.join(os.getcwd(), "mp3data")
-training_dir = os.path.join(os.getcwd(), "sample")
+training_dir = os.path.join(os.getcwd(), "mp3data", "preprocessed")
 mp_dict = {}
 fv_dict = {}
 
 # Read the dictionary.txt for easy hashing
+ndx = 0
 with open(os.path.join(data_dir, "dictionary.txt")) as f:
     for line in f:
         line = line.strip()
-        mp_dict[line] = 0
-        fv_dict[line] = 0
+        mp_dict[line] = ndx
+        ndx += 1
 
-# For all files, create a csv
+# Get the training set
+training_set = open(os.path.join(data_dir, "training_set.txt"), "r")
+test_set = open(os.path.join(data_dir, "test_set.txt"), "r")
+
+# Create the vectorizer
+vectorizer = CountVectorizer(vocabulary=mp_dict)
+
+## TRAINING SET
+# Create the list of file contents
+path_abs = lambda rel: open(os.path.join(training_dir, rel.strip()), "r").read()
+files = [path_abs(x) for x in test_set]
+rows = len(files)
+
+X = vectorizer.fit_transform(files)
+
+print("Done vectorizing, will now print to CSV")
+
+# Import to csv file
+fo = open(os.path.join(data_dir, "dataset-test.csv"), 'w')
+
+for r in range(rows):
+    row = X[r].toarray()[0]
+    #fo.write(list2str(X[r].toarray()[0].tolist()))
+    for c in range(len(row)):
+        if c > 0:
+            fo.write(',')
+        fo.write(str(row[c]))
+    fo.write('\n')
+    sys.stdout.write("\r{}/{}".format(r,rows))
+    sys.stdout.flush()
+
+fo.close()
+training_set.close()
+test_set.close()
--- training_sampler.py
+++ training_sampler.py
@@ -2,16 +2,22 @@ import os
 import sys
 
 # Create a .txt file containing the numbers of the emails to be used for the training set
+total = 75419
 n = 45252 # 60% of 75419
 
 # Create the file in the mp3data/ folder
 dest_dir = os.path.join(os.getcwd(), "mp3data")
 
 # Write to training_set.txt
-fo = open(os.path.join(dest_dir, "training_set.txt"), "w")
+training_set = open(os.path.join(dest_dir, "training_set.txt"), "w")
+test_set = open(os.path.join(dest_dir, "test_set.txt"), "w")
 
 # For each of the sampled numbers (in this case its only by order)
-for i in range(n):
-    fo.write("inmail.{}\n".format(i+1))
+for i in range(total):
+    if i < n:
+        training_set.write("inmail.{}\n".format(i+1))
+    else:
+        test_set.write("inmail.{}\n".format(i+1))
 
-fo.close()
+training_set.close()
+test_set.close()
